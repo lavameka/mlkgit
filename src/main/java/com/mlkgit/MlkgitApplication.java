73b58ed3d49015a96fb31d3ec3467e72a8efd9be
Lava kumar <lavakms@gmail.com>
2017-04-22 12:46:10 -0400

    initial commit for our first Spring boot project

---
+++ src/main/java/com/mlkgit/MlkgitApplication.java
@@ -0,0 +1,12 @@
+package com.mlkgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlkgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlkgitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mlkgit/MlkgitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mlkgit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MlkgitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
