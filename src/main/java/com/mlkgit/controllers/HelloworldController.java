64fcaefa3ea873db839833a80d9ea376a298c31e
Lava kumar <lavakms@gmail.com>
2017-04-24 17:22:24 -0400

    intial project commit

---
+++ src/main/java/com/mlkgit/controllers/HelloworldController.java
@@ -0,0 +1,19 @@
+package com.mlkgit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 4/24/2017.
+ */
+@Controller
+public class HelloworldController {
+
+    @RequestMapping("/")
+public String sayHello(){
+
+return"Index";
+}
+}
+
+
